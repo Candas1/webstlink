76de849e017bb88c96b803132eabbc8644c7834b
Candas1 <candascraig@hotmail.com>
2022-12-07 07:36:16 +0000

    Only keep Flashing feature for the demo

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 import * as libstlink from './src/lib/package.js';
 import WebStlink from './src/webstlink.js';
 import { hex_octet, hex_word, hex_octet_array } from './src/lib/util.js';
-import cs from './capstone-arm.min.js';
 
 function fetchResource(url) {
     return new Promise(function(resolve, reject) {
@@ -132,38 +131,6 @@ async function pick_sram_variant(mcu_list) {
     }
 }
 
-function update_registers(registers, explicit = false) {
-    for (let [name, value] of Object.entries(registers)) {
-        let span = document.getElementById(name);
-        let text = (name + ":").padEnd(5);
-        text += "0x" + value.toString(16).padStart(8,"0");
-        text += value.toString().padStart(12);
-
-        if (text != span.textContent && !span.textContent.endsWith("-")) {
-            span.classList.add("register-updated");
-        } else {
-            span.classList.remove("register-updated");
-        }
-        
-        span.textContent = text;
-    }
-
-    if (explicit) {
-        let registerDetails = document.getElementById("registerDisplay");
-        registerDetails.open = true;
-    }
-}
-
-function reset_registers() {
-    for (let span of document.querySelectorAll("span.register")) {
-        let name = span.id;
-        let text = (name + ":").padEnd(5);
-        text += "-".repeat(10) + "-".padStart(12);
-        span.classList.remove("register-updated");
-        span.textContent = text;
-    }
-}
-
 function update_debugger_info(stlink, device) {
     let probeInfo = document.getElementById("probeInfo");
     let summary = probeInfo.querySelector("summary");
@@ -220,136 +187,14 @@ document.addEventListener('DOMContentLoaded', event => {
     var curr_device = null;
 
     let log = document.querySelector("#log");
-    let logger = new libstlink.Logger(1, log);
-
-    document.querySelector("#logLevel").addEventListener('change', function(evt) {
-        logger.set_verbose(evt.target.value);
-        let desc = evt.target.nextSibling.textContent;
-        if (desc.indexOf("-") != -1) {
-            desc = desc.substring(0, desc.indexOf("-"));
-        }
-
-        this.querySelector("summary").textContent = "Logging Level - " + desc;
-    });
-
-    let pollingForm = document.getElementById("pollingForm");
-    let polling_mode = "on";
-    let polling_interval = 200;
-
-    async function poll_cpu() {
-        let summary = document.getElementById("pollingDisplay").querySelector("summary");
-
-        if (polling_mode == "off") {
-            summary.textContent = "Polling - off";
-        } else if (stlink !== null && stlink.connected && stlink.examined) {
-            let active = false;
-            let running = false;
-            let debuggable = false;
-            if (polling_mode == "always") {
-                summary.textContent = "Polling - active";
-                let status = await stlink.inspect_cpu();
-                running = !status.halted;
-                debuggable = status.debug;
-                active = true;
-            } else if (polling_mode == "on") {
-                let status = stlink.last_cpu_status;
-                if (status === null || !status.halted) {
-                    status = await stlink.inspect_cpu();
-                }
-                debuggable = status.debug;
-                if (!status.halted) {
-                    running = true;
-                    active = true;
-                }
-            }
-
-            if (active) {
-                summary.textContent = "Polling - active";
-                if (running && debuggable) {
-                    if (document.getElementById("pollRegisters").checked) {
-                        let registers = await stlink.read_registers();
-                        update_registers(registers);
-                    }
-                    if (document.getElementById("pollMemory").checked) {
-                        await read_and_display_memory(false);
-                    }
-                }
-            } else {
-                summary.textContent = "Polling - idle";
-            }
-        } else {
-            summary.textContent = "Polling - disconnected";
-        }
-    }
-
-    let polling_id = null;
-    function setup_polling(mode, interval) {
-        if (polling_id !== null) {
-            clearInterval(polling_id);
-            polling_id = null;
-        }
-
-        if (polling_mode != "off") {
-            polling_id = setInterval(poll_cpu, polling_interval);
-        }
-    }
-
-    pollingForm.addEventListener("change", async function (evt) {
-        let prev_mode = polling_mode;
-        let prev_interval = polling_interval;
-        polling_mode = pollingForm.elements["mode"].value;
-        polling_interval = parseInt(pollingForm.elements["interval"].value);
-        if ((prev_mode != polling_mode) || (prev_interval != polling_interval)) {
-            if (polling_mode == "off") {
-                let summary = document.getElementById("pollingDisplay").querySelector("summary");
-                summary.textContent = "Polling - off";
-            } else {
-                await poll_cpu();
-            }
-            setup_polling(polling_mode, polling_interval);
-        }
-    });
-
-    pollingForm.addEventListener("submit", prevent_submission);
-
-    setup_polling(polling_mode, polling_interval);
-
+    let logger = new libstlink.Logger(2, log);
+    
     let connectButton = document.querySelector("#connect");
-    let runHaltButton = document.querySelector("#runHalt");
-    let stepButton = document.querySelector("#step");
-    let resetButton = document.querySelector("#reset");
-    let readRegistersButton = document.querySelector("#readRegisters");
-    let readMemoryButton = document.querySelector("#readMemory");
     let flashButton = document.querySelector("#flash");
 
-    runHaltButton.addEventListener('click', async function() {
-        if (stlink !== null && stlink.connected) {
-            if (stlink.last_cpu_status.halted) {
-                await stlink.run();
-            } else {
-                await stlink.halt();
-            }
-        }
-    });
-
-    stepButton.addEventListener('click', async function() {
-        if (stlink !== null && stlink.connected) {
-            await stlink.step();
-        }
-    });
-
-    resetButton.addEventListener('click', async function() {
-        if (stlink !== null) {
-            await stlink.reset(stlink.last_cpu_status.halted);
-        }
-    });
-
-    readRegistersButton.addEventListener('click', async function(evt) {
-        if (stlink !== null && stlink.connected) {
-            let registers = await stlink.read_registers();
-            update_registers(registers, true);
-        }
-    });
+    window.setInterval(function() {
+        log.scrollTop = log.scrollHeight;
+      }, 500);
 
     async function read_and_display_memory(explicit = false) {
         if (stlink !== null && stlink.connected) {
@@ -364,53 +209,9 @@ document.addEventListener('DOMContentLoaded', event => {
             let memory = await stlink.read_memory(addr, size);
             let memoryContents = document.getElementById("memoryContents");
             memoryContents.textContent = hex_octet_array(memory).join(" ");
-            if (explicit) {
-                let memoryDetails = document.getElementById("memoryDisplay");
-                memoryDetails.open = true;
-            }
         }
     }
-
-    const mode = cs.MODE_THUMB + cs.MODE_MCLASS
-               + cs.MODE_LITTLE_ENDIAN;
-    let decoder = new cs.Capstone(cs.ARCH_ARM, mode);
-    async function disassemble_current_instruction() {
-        if (stlink !== null && stlink.connected) {
-            let pc = await stlink.read_register("PC");
-            const window_size = document.getElementById("disasmWindowSize").value;
-            const addr = (pc & 0xfffffffe);
-            const start_addr = addr - 2 * window_size;
-            const end_addr = addr + 2 + 2 * window_size;
-            let inst_bytes = await stlink.read_memory(start_addr, (end_addr - start_addr + 1));
-            let disasm = "";
-            try {
-                let instructions = decoder.disasm(inst_bytes, start_addr);
-                let lines = [];
-                for (let inst of instructions) {
-                    let line = hex_word(inst.address);
-                    if (inst.address == (pc & 0xfffffffe)) {
-                        line += "  → ";
-                    } else {
-                        line += "    ";
-                    }
-                    line += inst.mnemonic.padEnd(8);
-                    line += inst.op_str;
-                    lines.push(line);
-                }
-                disasm = lines.join("\n");
-            } catch (err) {
-                logger.error(`Failed to decode instructions [${hex_word(start_addr)}-${hex_word(end_addr)}]`)
-            }
-            let disp = document.getElementById("assemblyContents");
-            disp.textContent = disasm;
-            disp.rows = 1 + (2 * window_size);
-        }
-    }
-
-    readMemoryButton.addEventListener('click', function (evt) {
-        return read_and_display_memory(true);
-    });
-
+    
     flashButton.addEventListener('click', async function (evt) {
         if (stlink !== null && stlink.connected) {
             let addr_field = document.getElementById("flashWriteAddress");
@@ -424,11 +225,17 @@ document.addEventListener('DOMContentLoaded', event => {
             if (field.files.length > 0) {
                 let file = field.files[0];
                 let data = await read_file_as_array_buffer(file);
+                if (!stlink.last_cpu_status.halted) await stlink.halt();
+
                 try {
                     await stlink.flash(addr, data);
                 } catch (err) {
                     logger.error(err);
                     show_error_dialog(err);
+                }
+                finally{
+                    setTimeout(read_and_display_memory, 500,true); 
+                    await stlink.reset(stlink.last_cpu_status.halted);
                 }
             }
         }
@@ -437,62 +244,15 @@ document.addEventListener('DOMContentLoaded', event => {
     function update_capabilities(status) {
         if (status.debug) {
             if (status.halted) {
-                runHaltButton.textContent = "Run";
-                readRegistersButton.disabled = false;
-                readMemoryButton.disabled = false;
-                stepButton.disabled = false;
                 flashButton.disabled = false;
             } else {
-                runHaltButton.textContent = "Halt";
-                readRegistersButton.disabled = true;
-                readMemoryButton.disabled = true;
-                stepButton.disabled = true;
-                flashButton.disabled = true;
+                flashButton.disabled = false;
             }
-            runHaltButton.disabled = false;
-            resetButton.disabled = false;
         } else {
-            runHaltButton.disabled = true;
-            resetButton.disabled = true;
-            stepButton.disabled = true;
-            readRegistersButton.disabled = true;
-            readMemoryButton.disabled = true;
             flashButton.disabled = true;
         }
     }
-
-    let semihostingEnabled = document.getElementById("semihostingEnabled");
-    semihostingEnabled.addEventListener("change", function (evt) {
-        let summary = document.getElementById("semihostingDisplay").querySelector("summary");
-        summary.textContent = "Semihosting - " + (semihostingEnabled.checked ? "enabled" : "off");
-    });
-
-    let semihostingOutput = document.getElementById("semihostingOutput");
-    async function handle_semihosting() {
-        let handled = await stlink.handle_semihosting(oper => {
-            const opcodes = libstlink.semihosting.opcodes;
-            if (oper.opcode == opcodes.SYS_OPEN) {
-                // TODO: keep track of file handles
-                return 1;
-            } else if (oper.opcode == opcodes.SYS_WRITE) {
-                let msg = String.fromCharCode.apply(undefined, oper.data);
-                semihostingOutput.textContent += msg;
-                return 0;
-            } else if (oper.opcode == opcodes.SYS_FLEN) {
-                return 0;
-            } else if (oper.opcode == opcodes.SYS_ERRNO) {
-                return 0;
-            } else if (oper.opcode == opcodes.SYS_ISTTY) {
-                // TODO: keep track of file handles
-                return 1;
-            } else {
-                return -1;
-            }
-        });
-
-        return handled;
-    }
-
+    
     async function on_successful_attach(stlink, device) {
         // Export for manual debugging
         window.stlink = stlink;
@@ -500,7 +260,6 @@ document.addEventListener('DOMContentLoaded', event => {
 
         // Reset settings
         connectButton.textContent = "Disconnect";
-        reset_registers();
 
         // Populate debugger info
         update_debugger_info(stlink, device);
@@ -518,40 +277,6 @@ document.addEventListener('DOMContentLoaded', event => {
         // Detect attached target CPU
         let target = await stlink.detect_cpu([], pick_sram_variant);
 
-        // Attach the semihosting handler
-        stlink.add_callback('halted', async () => {
-            if (semihostingEnabled.checked) {
-                let semihosted = await handle_semihosting();
-                if (semihosted && !stlink.last_cpu_status.halted) {
-                    // Skip updating the UI when handling semihosting
-                    return false;
-                }
-            }
-        });
-
-        // Attach UI callbacks for whenever the CPU state is inspected
-        function update_on_inspection(status) {
-            // Update display
-            update_target_status(status, null);
-            // Update buttons
-            update_capabilities(status);
-        }
-
-        stlink.add_callback('halted', update_on_inspection);
-        stlink.add_callback('resumed', update_on_inspection);
-
-        // Handle auto-read-on-halt functionality
-        stlink.add_callback('halted', async () => {
-            if (document.getElementById("autoReadRegisters").checked) {
-                let registers = await stlink.read_registers();
-                update_registers(registers);
-            }
-            if (document.getElementById("autoReadMemory").checked) {
-                await read_and_display_memory(false);
-            }
-            await disassemble_current_instruction();
-        });
-
         // Update the UI with detected target info and debug state
         let status = await stlink.inspect_cpu();
         if (!status.debug) {
@@ -564,31 +289,31 @@ document.addEventListener('DOMContentLoaded', event => {
         update_capabilities(status);
 
         // Set the read memory address to the SRAM start
-        document.getElementById("memoryReadAddress").value = "0x" + hex_word(target.sram_start);
+        document.getElementById("memoryReadAddress").value = "0x" + hex_word(target.flash_start);
 
         // Set the flash write address to the Flash start
         document.getElementById("flashWriteAddress").value = "0x" + hex_word(target.flash_start);
+    
+        setTimeout(read_and_display_memory, 500,true);
     }
+
+    document.getElementById("memoryReadAddress").addEventListener('change', function (evt) {
+        return read_and_display_memory(true);
+    });
+
+    document.getElementById("memoryReadSize").addEventListener('change', function (evt) {
+        return read_and_display_memory(true);
+    });
 
     function on_disconnect() {
         logger.info("Device disconnected");
         connectButton.textContent = "Connect";
 
-        readRegistersButton.disabled = true;
-        readMemoryButton.disabled = true;
-        runHaltButton.disabled = true;
-        stepButton.disabled = true;
-        resetButton.disabled = true;
         flashButton.disabled = true;
 
         let probeInfo = document.getElementById("probeInfo");
         let summary = probeInfo.querySelector("summary");
         summary.textContent = "Debugger - Disconnected";
-
-        let pollingForm = document.getElementById("pollingForm");
-        let pollingInfo = document.getElementById("pollingDisplay");
-        summary = pollingInfo.querySelector("summary");
-        summary.textContent = "Polling - Idle";
 
         document.getElementById("productName").textContent = "";
         document.getElementById("mfgName").textContent = "";
